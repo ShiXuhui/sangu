ca86dbaf9f3e2daf1e4ab4a47fd48d1dbedba6a6
xzd <2432576662@qq.com>
2019-04-30 15:15:21 +0800

    内容实体类

---
+++ ly-framework-model/src/main/java/com/lingyun/framework/domain/content/Notice.java
@@ -0,0 +1,135 @@
+package com.lingyun.framework.domain.content;
+
+import java.io.Serializable;
+
+public class Notice implements Serializable{
+    private Integer id;
+
+    private Short typeId;
+
+    private String title;
+
+    private String titColor;
+
+    private Boolean titB;
+
+    private Byte isDisplay;
+
+    private String isUrl;
+
+    private String seoKeywords;
+
+    private String seoDescription;
+
+    private Integer click;
+
+    private Integer addtime;
+
+    private Short sort;
+
+    private String content;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Short getTypeId() {
+        return typeId;
+    }
+
+    public void setTypeId(Short typeId) {
+        this.typeId = typeId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title == null ? null : title.trim();
+    }
+
+    public String getTitColor() {
+        return titColor;
+    }
+
+    public void setTitColor(String titColor) {
+        this.titColor = titColor == null ? null : titColor.trim();
+    }
+
+    public Boolean getTitB() {
+        return titB;
+    }
+
+    public void setTitB(Boolean titB) {
+        this.titB = titB;
+    }
+
+    public Byte getIsDisplay() {
+        return isDisplay;
+    }
+
+    public void setIsDisplay(Byte isDisplay) {
+        this.isDisplay = isDisplay;
+    }
+
+    public String getIsUrl() {
+        return isUrl;
+    }
+
+    public void setIsUrl(String isUrl) {
+        this.isUrl = isUrl == null ? null : isUrl.trim();
+    }
+
+    public String getSeoKeywords() {
+        return seoKeywords;
+    }
+
+    public void setSeoKeywords(String seoKeywords) {
+        this.seoKeywords = seoKeywords == null ? null : seoKeywords.trim();
+    }
+
+    public String getSeoDescription() {
+        return seoDescription;
+    }
+
+    public void setSeoDescription(String seoDescription) {
+        this.seoDescription = seoDescription == null ? null : seoDescription.trim();
+    }
+
+    public Integer getClick() {
+        return click;
+    }
+
+    public void setClick(Integer click) {
+        this.click = click;
+    }
+
+    public Integer getAddtime() {
+        return addtime;
+    }
+
+    public void setAddtime(Integer addtime) {
+        this.addtime = addtime;
+    }
+
+    public Short getSort() {
+        return sort;
+    }
+
+    public void setSort(Short sort) {
+        this.sort = sort;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content == null ? null : content.trim();
+    }
+}
